c1d83f8c2ed4eac79bd89f19ee27326812304a1e
AhmedBarre10 <58369706+AhmedBarre10@users.noreply.github.com>
2020-04-17 14:07:07 -0400

    MVP

---
+++ model.js
@@ -0,0 +1,74 @@
+const db = require('./data/db-config.js');
+
+module.exports = {
+	findAllProjects,
+	findById,
+	findAllTasks,
+	findAllProjectResources,
+	addProject,
+	addTask,
+	// updateProject,
+	// updateTask,
+	// updateResources,
+	addResources,
+	removeProject
+	// removeTask,
+	// removeResources
+};
+
+async function findAllProjects() {
+	return db('projects');
+
+}
+
+function findById(id) {
+	return db('projects')
+		.where({ id })
+		.first();
+}
+
+function findAllTasks(projectId) {
+	return db('projects')
+		.select(
+			'projects.projects_name',
+			'tasks.notes',
+			'tasks.tasks_description',
+			'tasks.tasks_completed'
+		)
+		.join('tasks', 'tasks.project_id', 'projects.id')
+		.where('projects.id', projectId);
+}
+
+function findAllProjectResources(projectId) {
+	return db('projects')
+		.select(
+			'projects.projects_name',
+			'resources.resources_name',
+			'resources.resources_description'
+		)
+		.join('resources', 'resources.project_id', 'projects.id')
+		.where('projects.id', projectId);
+}
+
+function addProject(project) {
+	return db('projects').insert(project);
+}
+
+function addTask(task) {
+	return db('tasks').insert(task);
+}
+
+function addResources(resource) {
+	return db('resources').insert(resource);
+}
+
+// PUT REQUESTS
+
+function removeProject(id) {
+	return db('projects')
+		.where({ id })
+		.delete();
+}
+
+// REMOVE TASKS
+// REMOVE RESOURCES
